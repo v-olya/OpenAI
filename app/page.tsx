4c2a61630190947d03cbf94b2a8405a23d2d0cd4
Olya Vo <olga.volkova@email.cz>
2025-10-17 07:24:30 +0200

    replace the ancor texts

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
 import styles from './page.module.scss';
 
 const Home = () => {
-    const categories = {
-        Chat: 'basic-chat',
-        Weather: 'weather',
-        Search: 'news-search',
-    };
+    const categories = [
+        { url: 'basic-chat', title: 'Basic Chat' },
+        { url: 'weather', title: 'Weather Widget' },
+        { url: 'news-search', title: 'Today in News' },
+    ];
 
     return (
         <main className={styles.main}>
             <h2>Example of an app built with OpenAI API</h2>
             <div className='container home'>
-                {Object.entries(categories).map(([name, url]) => (
+                {categories.map(({ url, title }) => (
                     <a
-                        key={name}
+                        key={url}
                         className={`${styles.category} link-base`}
                         href={`/pages/${url}`}
                     >
-                        {name}
+                        {title}
                     </a>
                 ))}
             </div>
