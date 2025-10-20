1bef2781058b49e26f36176d737e46a83fe633e7
Olya Vo <olga.volkova@email.cz>
2025-10-20 13:05:51 +0200

    add a resetChat button

--- app/components/chat/chat-layout.tsx
+++ app/components/chat/chat-layout.tsx
@@ -3,23 +3,46 @@
 import Link from 'next/link';
 import type { ReactNode } from 'react';
 import chatStyles from './chat.module.scss';
+import { ChatProps } from '@/utils/types';
+import { dispatchNewChat } from '@/utils/dispatch-new-chat';
 
 interface ChatLayoutProps {
     children?: ReactNode;
     messages: ReactNode;
     inputForm: ReactNode;
+    chatType?: ChatProps['chatType'];
 }
 
-export function ChatLayout({ children, messages, inputForm }: ChatLayoutProps) {
+export function ChatLayout({
+    children,
+    messages,
+    inputForm,
+    chatType,
+}: ChatLayoutProps) {
     return (
         <div className={chatStyles.chat}>
-            <Link
-                href='/'
-                className={`${chatStyles['home-link']} home-link link-base`}
-                aria-label='Home'
-            >
-                Home
-            </Link>
+            <div className={chatStyles['menu-links']}>
+                <Link
+                    href='/'
+                    className={`${chatStyles['menu-link']} menu-link`}
+                    aria-label='Home'
+                >
+                    Home
+                </Link>
+                {chatType === 'coding' && (
+                    <Link
+                        href=':'
+                        className={`${chatStyles['menu-link']} menu-link`}
+                        aria-label='Restart the chat'
+                        onClick={(e) => {
+                            e.preventDefault();
+                            dispatchNewChat({ text: '' });
+                        }}
+                    >
+                        Restart
+                    </Link>
+                )}
+            </div>
             <div className={chatStyles.messages}>
                 {messages}
                 {children}
